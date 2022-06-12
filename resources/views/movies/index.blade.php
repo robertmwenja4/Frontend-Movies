c7eb04838da152014fb055734f4d3c22a9e2cefd
robertmwenja4 <robertmwenja4@gmail.com>
2022-06-12 21:27:22 +0300

    Uploading Pictures

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,11 +4,30 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-12">
+            @if (session('status'))
+                <h6 class="alert alert-success">{{ session('status') }}</h6>
+            @endif
             <div class="card">
                 <div class="card-header">
                     <h4>Add Movie
                         <a href="{{ url('') }}" class="btn btn-danger float-end">BACK</a>
                     </h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('add-movie') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="">Movie Title:</label>
+                            <input type="text" name="title" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Movie Image</label>
+                            <input type="file" name="movie_image" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [MovieModelController::class, 'index']);
 Route::get('/index', [UserController::class,'index']);
+Route::post('/add-movie', [MovieModelController::class, 'store']);
